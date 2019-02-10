d46ff2fe944cc48ae9f9657fb10504bcd4b73a15
Michał Kołodziej <asezrik@gmail.com>
2019-02-10 16:33:04 +0100

    nsp_update

--- nsp/nsp-script.js
+++ nsp/nsp-script.js
@@ -1,5 +1,11 @@
 const conf = {
     offsetTime: 1000, //A number of miliseconds that u have to wait to scroll page again
+
+    //Change 'true' to 'false' if u don't want one of these option of scrolling to be active
+    canScrollWithKey: true,
+    canScrollWithMouseWheel: true,
+    canScrollWithDotClick: true,
+    canScrollWithFingerSlide: true, //mobile
 }
 
 class Section {
@@ -94,15 +100,6 @@ else {
         }, conf.offsetTime);
     }
 
-    const handleWheelEvent = (e) => {
-        if (canBeDone) {
-            clicked = false;
-            changeSection(e);
-            canBeDone = false;
-
-            resolveAfterSomeTime();
-        }
-    }
 
     // Function that manages changing sections
     const changeSection = (e) => {
@@ -139,51 +136,105 @@ else {
 
     }
 
-    let touchClientYStart = 0;
-    let touchClientYEnd = 0;
+    //A part of code that handles scrolling the page by using 'finger slide' (mobile)
+    if (conf.canScrollWithFingerSlide) {
+        let touchClientYStart = 0;
+        let touchClientYEnd = 0;
 
-    const touchedStart = (e) => {
-        touchClientYStart = e.changedTouches[0].clientY;
+        const touchedStart = (e) => {
+            touchClientYStart = e.changedTouches[0].clientY;
+        }
+
+        const touchedEnd = (e) => {
+            touchClientYEnd = e.changedTouches[0].clientY;
+
+            if (canBeDone) {
+                if (touchClientYEnd < touchClientYStart && touchClientYStart - touchClientYEnd > 15) {
+                    clicked = false;
+                    const e = { deltaY: 1 };
+                    changeSection(e);
+                }
+                else if (touchClientYEnd > touchClientYStart && touchClientYEnd - touchClientYStart > 15) {
+                    clicked = false;
+                    const e = { deltaY: -1 };
+                    changeSection(e);
+                }
+                canBeDone = false;
+
+                resolveAfterSomeTime();
+            }
+        }
+
+        document.addEventListener('touchstart', touchedStart);
+        document.addEventListener('touchend', touchedEnd);
     }
 
-    const touchedEnd = (e) => {
-        touchClientYEnd = e.changedTouches[0].clientY;
-
-        if (canBeDone) {
-            if (touchClientYEnd < touchClientYStart && touchClientYStart - touchClientYEnd > 15) {
-                const e = { deltaY: 1 };
+    //A part of code that handles scrolling the page by using mousewheel
+    if (conf.canScrollWithMouseWheel) {
+        const handleWheelEvent = (e) => {
+            if (canBeDone) {
+                clicked = false;
                 changeSection(e);
-            }
-            else if (touchClientYEnd > touchClientYStart && touchClientYEnd - touchClientYStart > 15) {
-                const e = { deltaY: -1 };
-                changeSection(e);
-            }
-            canBeDone = false;
+                canBeDone = false;
 
-            resolveAfterSomeTime();
+                resolveAfterSomeTime();
+            }
+        }
+
+        document.addEventListener('wheel', handleWheelEvent);
+    }
+
+    //A part of code that handles scrolling the page by pressing up or down arrow key
+    if (conf.canScrollWithKey) {
+
+        const handleKeyDownEvent = (e) => {
+            console.log(canBeDone, e);
+            let x;
+            if (canBeDone) {
+                if (e.keyCode === 38) {
+                    clicked = false;
+                    x = { deltaY: -1 };
+                    changeSection(x);
+                    canBeDone = false;
+                    resolveAfterSomeTime();
+                }
+                else if (e.keyCode === 40) {
+                    clicked = false;
+                    x = { deltaY: 1 };
+                    changeSection(x);
+                    canBeDone = false;
+                    resolveAfterSomeTime();
+                }
+            }
+        }
+
+        document.addEventListener('keydown', handleKeyDownEvent);
+    }
+
+    //Part of code that handles scrolling the page by clicking on dots
+    if (conf.canScrollWithDotClick) {
+        const handleDotClick = (e) => {
+            if (canBeDone) {
+                clicked = true;
+                targetDot = e.target.dataset.nspIndex;
+                toggleDots(targetDot);
+                canBeDone = false;
+
+                resolveAfterSomeTime()
+            }
+        }
+
+        if (areThereDots) {
+            dots.forEach(dot => {
+                dot.addEventListener('click', handleDotClick);
+                dot.addEventListener('touchstart', handleDotClick);
+            });
         }
     }
-
-
-    document.addEventListener('wheel', handleWheelEvent);
-    document.addEventListener('touchstart', touchedStart);
-    document.addEventListener('touchend', touchedEnd);
-
-    const handleDotClick = (e) => {
-        if (canBeDone === true) {
-            clicked = true;
-            targetDot = e.target.dataset.nspIndex;
-            toggleDots(targetDot);
-            canBeDone = false;
-
-            resolveAfterSomeTime()
-        }
-    }
-    if (areThereDots) {
-        dots.forEach(dot => {
-            dot.addEventListener('click', handleDotClick);
-            dot.addEventListener('touchstart', handleDotClick);
-        });
-    }
-
 }
+
+
+// TODO:
+// 
+// 
+// 
